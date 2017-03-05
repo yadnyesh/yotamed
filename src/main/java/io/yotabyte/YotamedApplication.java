182b79e31550a3233d9ae8df69652bb542b0b4af
yotabyteyad <yotabyte.yad@gmail.com>
2017-03-05 09:53:52 +0530

    First Commit

---
+++ src/main/java/io/yotabyte/YotamedApplication.java
@@ -0,0 +1,12 @@
+package io.yotabyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YotamedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YotamedApplication.class, args);
+	}
+}
